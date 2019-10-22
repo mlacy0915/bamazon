1691c04055f52df1908025e360563796d6ba05a5
Maliah Lacy <mlacy0915@gmail.com>
2019-10-21 20:33:24 -0500

    Products and values added

--- products.sql
+++ products.sql
@@ -1,12 +1,26 @@
-DROP DATABASE IF products.sql;
 CREATE DATABASE products.sql
 
-USE animals_db;
+USE products.sql;
 
 CREATE TABLE Products(
-    item_id VARCHAR(),
-    product_name VARCHAR(),
-    department_name VARCHAR(),
+    item_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(30),
+    department_name VARCHAR(30),
     price INTEGER(),
     stock_quantity INTEGER(),
 ) 
+
+INSERT INTO Products(product_name, department_name, price, stock_quantity)
+VALUES ("iPad Pro," "electronics", 999.99, 100 ),
+("Christian Louboutin-Marie Jane Bag", "accesories", 2590.00, 50),
+("Kate Spade Smart Watch", "accesories", 165.00, 33 ),
+("Game of Thrones Complete Set", "Blu-ray", 249.99, 78),
+("Bad Girl Throughout History", "books", 13.58, 217),
+("Super Nintendo", "electronics", 149.99, 63),
+("CoCo & Co Coconut Oil", "beauty", 39.99, 52),
+("Vegan Vitamin D3", "health", 14.95, 14),
+("Capital City Mambo Sauce", "grocery", 13.00, 30),
+("Little Seeds Piper Crib", "baby", 350.00, 36),
+
+
+
