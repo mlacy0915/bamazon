f4c75f53b2dc013f704a11428e4387477be67373
Maliah Lacy <mlacy0915@gmail.com>
2019-10-21 21:07:58 -0500

    MySQL connection added

---
+++ bamazonCustomer.js
@@ -0,0 +1,12 @@
+var prompt = require('prompt');
+var mysql = require('mysql');
+var padText = require('./padTable.js')
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: ,
+    user: "mlacy0915",
+    password: "Kisses@12",
+    database: "bamazon"
+});
+
