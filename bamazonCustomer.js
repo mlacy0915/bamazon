db342460622c5c3d453e0575cea1b628c8f6bafc
Maliah Lacy <mlacy0915@gmail.com>
2019-10-22 19:39:19 -0500

    table to display items inside database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,7 +17,27 @@ connection.connect(function(err) {
 
 connection.query("SELECT * FROM Products", function(err, res) {
     if(err) throw err;
-
-    console.log("Come Shop With Us!");
 })
 
+    console.log("Shop our selection...\n");
+
+    console.log('  ID  |      Product Name      |  Department Name  |   Price  | In Stock');
+    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+
+    for(var i = 0; i < res.length; i++){
+        var itemID = res[i].ItemID + '';
+        itemID = padText("  ID  ", itemID);
+
+        var productName = res[i].ProductName + '';
+        productName = padText("      Product Name      ", productName);
+    
+        var departmentName = res[i].DepartmentName + '';
+        departmentName = padText("  Department Name  ", departmentName);
+    
+        var price = '$' + res[i].Price.toFixed(2) + ''; 
+        price = padText("   Price  ", price);
+    
+        var quantity = res[i].StockQuantity + '';
+
+        console.log(itemID + '|' + productName + '|' + departmentName + '|' + price + '|    ' + quantity);
+  }
